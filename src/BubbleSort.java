aa9d036cd906c0e431f51da95e109dfc7966f970
tanu-11sharma <shartana.01@gmail.com>
2023-08-14 19:45:07 +0530

    add j print

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -4,6 +4,7 @@ public class BubbleSort {
         for(int i = 0 ; i< n;i++){
             for(int j = i+1; i<n-1;j++){
                 System.out.println(i);
+                System.out.print(j);
             }
         }
     }
